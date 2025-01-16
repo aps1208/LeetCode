7ac4a03198fd47cf5a620e0865bfd89afaf81dee
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-01-16 21:27:36 +0530

    Time: 11 ms (44.45%) | Memory: 46.1 MB (76.77%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -5,21 +5,21 @@ class Solution {
         {
             freq[i-'a']++;
         }
+        int result=0;
         for(int i=0;i<26;i++)
         {
             if(freq[i]>2 && freq[i]%2==0)
             {
-                freq[i]=2;
+                result+=2;
             }
             else if(freq[i]>2 && freq[i]%2==1)
             {
-                freq[i]=1;
+                result+=1;
             }
-        }
-        int result=0;
-        for(int i:freq)
-        {
-            result+=i;
+            else if(freq[i]<=2)
+            {
+                result+=freq[i];
+            }
         }
         return result;
     }
