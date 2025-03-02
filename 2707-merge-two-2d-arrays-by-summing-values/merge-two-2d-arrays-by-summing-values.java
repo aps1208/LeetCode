0e4ac15a99dc6afd7802f4077fe37f98bb679800
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-03-02 09:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (88.77%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,29 +1,39 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int max=0;
-        for(int i=0;i<nums1.length;i++) 
+        ArrayList<int[]> result=new ArrayList<>();
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length)
         {
-            max=Math.max(max,nums1[i][0]);
-            map.put(nums1[i][0],nums1[i][1]);
-        }
-        for(int i=0;i<nums2.length;i++) 
-        {
-            max=Math.max(max,nums2[i][0]);
-            map.put(nums2[i][0],map.getOrDefault(nums2[i][0],0)+nums2[i][1]);
-        }
-        int[][] result=new int[map.size()][2];
-        int k=0;
-        for(int i=1;i<max+1;i++)
-        {
-            if(map.containsKey(i))
+            if(nums1[i][0]==nums2[j][0])
             {
-                result[k][0]=i;
-                result[k][1]=map.get(i);
-                k++;
+                int[] temp=new int[2];
+                temp[0]=nums1[i][0];
+                temp[1]=nums1[i][1]+nums2[j][1];
+                result.add(temp);
+                i++;
+                j++;
+            }
+            else if(nums1[i][0]<nums2[j][0])
+            {
+                result.add(nums1[i]);
+                i++;
+            }
+            else
+            {
+                result.add(nums2[j]);
+                j++;
             }
         }
-
-        return result;
+        while(i<nums1.length)
+        {
+            result.add(nums1[i]);
+            i++;
+        }
+        while(j<nums2.length)
+        {
+            result.add(nums2[j]);
+            j++;
+        }
+        return result.toArray(new int[result.size()][]);
     }
 }
