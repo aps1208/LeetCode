215088d3386cd25766db9eb9a5bae364de3fc608
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-03-02 09:38:03 +0530

    Time: 5 ms (28.44%) | Memory: 45.7 MB (8.23%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,17 +1,29 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums1.length;i++) map.put(nums1[i][0],nums1[i][1]);
-        for(int i=0;i<nums2.length;i++) map.put(nums2[i][0],map.getOrDefault(nums2[i][0],0)+nums2[i][1]);
+        int max=0;
+        for(int i=0;i<nums1.length;i++) 
+        {
+            max=Math.max(max,nums1[i][0]);
+            map.put(nums1[i][0],nums1[i][1]);
+        }
+        for(int i=0;i<nums2.length;i++) 
+        {
+            max=Math.max(max,nums2[i][0]);
+            map.put(nums2[i][0],map.getOrDefault(nums2[i][0],0)+nums2[i][1]);
+        }
         int[][] result=new int[map.size()][2];
         int k=0;
-        for(int i:map.keySet())
+        for(int i=1;i<max+1;i++)
         {
-            result[k][0]=i;
-            result[k][1]=map.get(i);
-            k++;
+            if(map.containsKey(i))
+            {
+                result[k][0]=i;
+                result[k][1]=map.get(i);
+                k++;
+            }
         }
-        Arrays.sort(result, (a,b)->Integer.compare(a[0],b[0]));
+
         return result;
     }
 }
