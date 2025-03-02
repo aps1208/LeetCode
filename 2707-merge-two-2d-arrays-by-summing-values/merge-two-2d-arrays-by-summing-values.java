b0e3390805a634216b498989a9cb893265031f0c
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-03-02 09:30:47 +0530

    Time: 5 ms (28.44%) | Memory: 45.6 MB (31.44%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,47 +1,17 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums1.length;i++) set.add(nums1[i][0]);
-        for(int i=0;i<nums2.length;i++) set.add(nums2[i][0]);
-        int[][] result=new int[set.size()][2];
-        int i=0,j=0,k=0;
-        while(i<nums1.length && j<nums2.length)
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++) map.put(nums1[i][0],nums1[i][1]);
+        for(int i=0;i<nums2.length;i++) map.put(nums2[i][0],map.getOrDefault(nums2[i][0],0)+nums2[i][1]);
+        int[][] result=new int[map.size()][2];
+        int k=0;
+        for(int i:map.keySet())
         {
-            if(nums1[i][0]==nums2[j][0])
-            {
-                result[k][0]=nums1[i][0];
-                result[k][1]=nums1[i][1]+nums2[j][1];
-                i++;
-                j++;
-            }
-            else if(nums1[i][0]<nums2[j][0])
-            {
-                result[k][0]=nums1[i][0];
-                result[k][1]=nums1[i][1];
-                i++;
-            }
-            else
-            {
-                result[k][0]=nums2[j][0];
-                result[k][1]=nums2[j][1];
-                j++;
-            }
+            result[k][0]=i;
+            result[k][1]=map.get(i);
             k++;
         }
-        while(i<nums1.length)
-        {
-            result[k][0]=nums1[i][0];
-            result[k][1]=nums1[i][1];
-            i++;
-            k++;
-        }
-        while(j<nums2.length)
-        {
-            result[k][0]=nums2[j][0];
-            result[k][1]=nums2[j][1];
-            j++;
-            k++;
-        }
+        Arrays.sort(result, (a,b)->Integer.compare(a[0],b[0]));
         return result;
     }
 }
