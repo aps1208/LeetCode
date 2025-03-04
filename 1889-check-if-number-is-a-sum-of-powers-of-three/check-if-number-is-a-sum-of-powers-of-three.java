a84a1d3b3440f916f889aa160a1be5c0acc661f8
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-03-04 22:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (9.30%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,21 +1,6 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        int[] arr=new int[17];
-        while(n>0)
-        {
-            int temp=1,cnt=0;
-            while(temp*3<=n)
-            {
-                cnt++;
-                temp*=3;
-            }
-            if(arr[cnt]==0)
-            {
-                n-=temp;
-                arr[cnt]++;
-            }
-            else return false;
-        }
-        return true;
+        String str=Integer.toString(n,3);
+        return !str.contains("2");
     }
 }
