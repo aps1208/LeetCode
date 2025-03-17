20e4d1495db4ba1d93cf7a2b7a9b89593e6495ce
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-03-17 23:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (66.13%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,21 +1,15 @@
 class Solution {
-    public int help(int[] nums, int l, int h)
-    {
-        if(l>h) return -1;
-        int mid=(l+h)/2;
-        if((mid-1>=0 && nums[mid-1]!=nums[mid]) && (mid+1<nums.length && nums[mid+1]!=nums[mid]))
-        {
-            return nums[mid];
-        }
-        if(mid-1<0 && nums[mid+1]!=nums[mid]) return nums[mid];
-        if(mid+1>=nums.length && nums[mid-1]!=nums[mid]) return nums[mid];
-        int left=help(nums,l,mid-1);
-        int right=help(nums,mid+1,h);
-        if(left!=-1) return left;
-        return right;
-    }
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length==1) return nums[0];
-        return help(nums,0,nums.length-1);
+        int l=0,h=nums.length-1;
+        while(l<=h)
+        {
+            int mid=(l+h)/2, left=mid, right=mid;
+            if(mid-1>=0 && nums[mid-1]==nums[mid]) left--;
+            if(mid+1<nums.length && nums[mid+1]==nums[mid]) right++;
+            if(left==right) return nums[mid];
+            else if(left%2!=0) h=mid-1;
+            else l=mid+1;
+        }
+        return 0;
     }
 }
