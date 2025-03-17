d6dde440882741ee82186c61ee3d6b7b9b88f3bd
Aman Pratap Singh <142490467+aps1208@users.noreply.github.com>
2025-03-17 23:26:34 +0530

    Time: 1 ms (27.01%) | Memory: 50.2 MB (66.13%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,7 +1,21 @@
 class Solution {
+    public int help(int[] nums, int l, int h)
+    {
+        if(l>h) return -1;
+        int mid=(l+h)/2;
+        if((mid-1>=0 && nums[mid-1]!=nums[mid]) && (mid+1<nums.length && nums[mid+1]!=nums[mid]))
+        {
+            return nums[mid];
+        }
+        if(mid-1<0 && nums[mid+1]!=nums[mid]) return nums[mid];
+        if(mid+1>=nums.length && nums[mid-1]!=nums[mid]) return nums[mid];
+        int left=help(nums,l,mid-1);
+        int right=help(nums,mid+1,h);
+        if(left!=-1) return left;
+        return right;
+    }
     public int singleNonDuplicate(int[] nums) {
-        int sum=0;
-        for(int i:nums) sum^=i;
-        return sum;
+        if(nums.length==1) return nums[0];
+        return help(nums,0,nums.length-1);
     }
 }
